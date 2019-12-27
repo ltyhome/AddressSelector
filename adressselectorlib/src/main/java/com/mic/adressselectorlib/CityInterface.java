da23a3cd9fff0110d41b0cace90766a1177cc57b
梁蕾 <lianglei@tojoy.com>
2019-12-27 14:48:19 +0800

    1

--- adressselectorlib/src/main/java/com/mic/adressselectorlib/CityInterface.java
+++ adressselectorlib/src/main/java/com/mic/adressselectorlib/CityInterface.java
@@ -8,4 +8,5 @@ package com.mic.adressselectorlib;
 
 public interface CityInterface {
     String getCityName();
+    String getCityCode();
 }
--- app/src/main/java/com/mic/addressselectordemo/City.java
+++ app/src/main/java/com/mic/addressselectordemo/City.java
@@ -9,45 +9,16 @@ import com.mic.adressselectorlib.CityInterface;
  */
 
 public class City implements CityInterface{
-    private String Name;
-    private String id;
-    private String Grade;
-    private String IsMunicipality;
+    private String n;
+    private String v;
 
-    public String getIsMunicipality() {
-        return IsMunicipality;
-    }
-
-    public void setIsMunicipality(String isMunicipality) {
-        IsMunicipality = isMunicipality;
-    }
-
-    public String getName() {
-        return Name;
-    }
-
-    public void setName(String name) {
-        Name = name;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getGrade() {
-        return Grade;
-    }
-
-    public void setGrade(String grade) {
-        Grade = grade;
+    @Override
+    public String getCityCode() {
+        return v;
     }
 
     @Override
     public String getCityName() {
-        return Name;
+        return n;
     }
 }
